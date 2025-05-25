233f7f6fd9a32aa1c1c08bea5e758f99a9bc1fe1
VidyaSud <vidya.lokesh.j@gmail.com>
2025-05-24 20:18:14 -0400

    vs code update

--- trigger.js
+++ trigger.js
@@ -3,3 +3,4 @@ console.log("MAIN");
 console.log("Code added from March2Onetrust branch");
 const sum=2+4;
 console.log("Airtable changes are here",sum);
+console.log("added from develop in VS code");
