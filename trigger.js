b7b503ad1d5467c8ad188d45fd6b2ae3787e9a9f
VidyaSud <33089457+VidyaSud@users.noreply.github.com>
2025-05-24 21:22:19 -0400

    Update trigger.js

--- trigger.js
+++ trigger.js
@@ -4,4 +4,4 @@ console.log("Code added from March2Onetrust branch");
 const sum=2+4;
 console.log("Airtable changes are here",sum);
 console.log("added from develop in VS code");
-console.log("feture branch resources");
+console.log("Confident in GIT");
